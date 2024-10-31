a6e064c4e004d6cdbf0aa2e0c9fa0752cab310a9
Clemens Zagler <c.zagler@noi.bz.it>
2024-10-31 13:51:22 +0100

    initial import of transmodel API ninja module

---
+++ ninja/customdto.go
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+
+// SPDX-License-Identifier: MPL-2.0
+
+package ninja
+
+type OdhStation[T any] struct {
+	Scode   string
+	Sname   string
+	Sorigin string
+	Scoord  OdhCoord `json:"scoordinate"`
+	Smeta   T        `json:"smetadata"`
+}
+
+type OdhCoord struct {
+	X    float32
+	Y    float32
+	Srid uint32
+}
+
+type MetaAny map[string]any
+
+type OdhLatest struct {
+	MPeriod    int       `json:"mperiod"`
+	MValidTime NinjaTime `json:"mvalidtime"`
+	MValue     int       `json:"mvalue"`
+	Scode      string    `json:"scode"`
+	Stype      string    `json:"stype"`
+	Tname      string    `json:"tname"`
+}
---
+++ ninja/testing.go
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+
+// SPDX-License-Identifier: MPL-2.0
+
+package ninja
+
+import (
+	"encoding/json"
+	"os"
+	"reflect"
+)
+
+// For testing purposes, set this function and it will be used to retrieve a request instead of http
+var TestReqHook func(*NinjaRequest) (any, error)
+
+func runReqHook(req *NinjaRequest, result any) error {
+	r, err := TestReqHook(req)
+	if err != nil {
+		return err
+	}
+	// unholy memcpy: sets the memory at result to the value of r.
+
+	// They have to be the same type
+	// Has to be this way, because golang does not allow for variables to hold parametrized functions.
+	// So we have to use 'any' and hack around it with reflection. The golang.json lib does the same
+	reflect.ValueOf(result).Elem().Set(reflect.ValueOf(r).Elem())
+	return nil
+}
+
+func LoadJsonFile[T any](f string) (*NinjaResponse[T], error) {
+	r := &NinjaResponse[T]{}
+	b, err := os.ReadFile(f)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(b, r)
+	return r, nil
+}
